185e2152394084c022a11b6daa189ef4ca849cea
CallMeDale <hzf379039811@qq.com>
2017-05-19 10:45:25 +0800

    Update Handle2Impl.java

--- src/main/java/com/dale/ms/handle/Handle2Impl.java
+++ src/main/java/com/dale/ms/handle/Handle2Impl.java
@@ -5,27 +5,9 @@ package com.dale.ms.handle;
 
 import java.util.Map;
 
-<<<<<<< HEAD
 import com.dale.ms.entities.HmStore;
 import com.dale.ms.service.StoreService;
 import com.dale.ms.service.impl.StoreServiceImpl;
-=======
-<<<<<<< HEAD
-import com.dale.ms.entities.HmStore;
-import com.dale.ms.service.StoreService;
-import com.dale.ms.service.impl.StoreServiceImpl;
-=======
-<<<<<<< HEAD
-import com.dale.ms.entities.HmStore;
-import com.dale.ms.service.StoreService;
-import com.dale.ms.service.impl.StoreServiceImpl;
-=======
-import com.dale.ms.entities.HmUser;
-import com.dale.ms.service.StoreService;
-import com.dale.ms.service.impl.MainServiceImpl;
->>>>>>> 6655372f9e8d6c6c58bddc39817e4fdfc5aad381
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 import com.dale.ms.utils.GlobalUtil;
 import com.dale.ms.utils.MyLogUtil;
 
@@ -36,29 +18,13 @@ import com.dale.ms.utils.MyLogUtil;
  */
 public class Handle2Impl extends BaseHandle implements HandleInterface{
 
-<<<<<<< HEAD
 	private StoreServiceImpl storeService;
-=======
-<<<<<<< HEAD
-	private StoreServiceImpl storeService;
-=======
-	private StoreService storeService;
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 	private int cmd;
 	private String resultData;
 	
 	
 	
-<<<<<<< HEAD
 	public Handle2Impl(Map<String, Object> contents, int cmd, StoreServiceImpl storeService) {
-=======
-<<<<<<< HEAD
-	public Handle2Impl(Map<String, Object> contents, int cmd, StoreServiceImpl storeService) {
-=======
-	public Handle2Impl(Map<String, Object> contents, int cmd, StoreService storeService) {
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 		this.contents = contents;
 		this.cmd = cmd;
 		this.storeService = storeService;
@@ -68,13 +34,6 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 	 *   商家程序入口
 	 */
 	public void parseContent() {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 		HmStore store = (HmStore) object;
 		switch (cmd) {
 		case GlobalUtil.CMD_1:
@@ -97,33 +56,6 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 			break;
 		case GlobalUtil.CMD_7:
 			getOrderList(store);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-		HmUser user = (HmUser) object;
-		switch (cmd) {
-		case GlobalUtil.CMD_1:
-			regist(user);
-			break;
-		case GlobalUtil.CMD_2:
-			login(user);
-			break;
-		case GlobalUtil.CMD_3:
-			storeDetail(user);
-			break;
-		case GlobalUtil.CMD_4:
-			operateStore(user);
-			break;
-		case GlobalUtil.CMD_5:
-			QRCode(user);
-			break;
-		case GlobalUtil.CMD_6:
-			feedback(user);
->>>>>>> 6655372f9e8d6c6c58bddc39817e4fdfc5aad381
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 			break;
 		default:
 			MyLogUtil.print("未知 CMD，执行默认方法!");
@@ -136,19 +68,7 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 	 * 申请入驻
 	 * @param user
 	 */
-<<<<<<< HEAD
 	private void regist(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void regist(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void regist(HmStore store) {
-=======
-	public void regist(HmUser user) {
->>>>>>> 6655372f9e8d6c6c58bddc39817e4fdfc5aad381
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 		
 	}
 	
@@ -156,19 +76,7 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 	 * 短信登录
 	 * @param user
 	 */
-<<<<<<< HEAD
 	private void login(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void login(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void login(HmStore store) {
-=======
-	public void login(HmUser user) {
->>>>>>> 6655372f9e8d6c6c58bddc39817e4fdfc5aad381
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 		
 	}
 	
@@ -176,19 +84,7 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 	 * 获取店铺资料
 	 * @param user
 	 */
-<<<<<<< HEAD
 	private void storeDetail(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void storeDetail(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void storeDetail(HmStore store) {
-=======
-	public void storeDetail(HmUser user) {
->>>>>>> 6655372f9e8d6c6c58bddc39817e4fdfc5aad381
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 		
 	}
 	
@@ -196,19 +92,7 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 	 * 修改店铺资料
 	 * @param user
 	 */
-<<<<<<< HEAD
 	private void operateStore(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void operateStore(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void operateStore(HmStore store) {
-=======
-	public void operateStore(HmUser user) {
->>>>>>> 6655372f9e8d6c6c58bddc39817e4fdfc5aad381
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 		
 	}
 	
@@ -216,19 +100,7 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 	 * 获取店铺二维码
 	 * @param user
 	 */
-<<<<<<< HEAD
 	private void QRCode(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void QRCode(HmStore store) {
-=======
-<<<<<<< HEAD
-	private void QRCode(HmStore store) {
-=======
-	public void QRCode(HmUser user) {
->>>>>>> 6655372f9e8d6c6c58bddc39817e4fdfc5aad381
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 		
 	}
 	
@@ -236,13 +108,6 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 	 * 意见与反馈
 	 * @param user
 	 */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 	private void feedback(HmStore store) {
 		
 	}
@@ -263,18 +128,6 @@ public class Handle2Impl extends BaseHandle implements HandleInterface{
 	
 	
 	
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-	public void feedback(HmUser user) {
-		
-	}
-	
->>>>>>> 6655372f9e8d6c6c58bddc39817e4fdfc5aad381
->>>>>>> 8b524b034893a58123136e072f9c2d5db6b0173c
->>>>>>> e29539dff60f85419c4469ca27c1b309769013f3
 	
 	
 	
